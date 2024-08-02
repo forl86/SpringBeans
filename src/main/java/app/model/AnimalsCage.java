e972a9404c47388b46f4e7785e7845302c508b3d
Sergey T <forl86@gmail.com>
2024-08-03 00:44:01 +0800

    first commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,26 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(this.timer.getTime());
+        System.out.println("________________________");
+    }
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return this.timer;
+    }
+}
---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
